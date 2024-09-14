acb6bcaad126e03f1ee11968840b9c9f3a2982b5
studentqa2023 <rezwanislam857@gmail.com>
2024-09-14 14:30:53 -0400

    Added  NewClass under com.generic

---
+++ src/main/java/com/generic/NewClass.java
@@ -0,0 +1,9 @@
+package com.generic;
+
+public class NewClass {
+	
+	public static void main(String[] args) {
+		System.out.println("This is a  new  clasafter the initial push to GitHub.");
+	}
+
+}
